8d023e13645ab45bbdd7c1167f1f8028707b791f
Rettertyp <marius@kaufmanns.de>
2023-10-09 22:40:11 +0200

    migrate to nx monorepo

---
+++ apps/anamatic/src/app/letters-display/letters-display.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LettersDisplayComponent } from './letters-display.component';
+
+describe('LettersDisplayComponent', () => {
+  let component: LettersDisplayComponent;
+  let fixture: ComponentFixture<LettersDisplayComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LettersDisplayComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LettersDisplayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
